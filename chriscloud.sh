793c29bb23b2cd7e30408608f2e2ec01719bbafa
icephonix <43659237+icephonix@users.noreply.github.com>
2020-09-16 13:29:37 +0800

    Update chriscloud.sh

--- chriscloud.sh
+++ chriscloud.sh
@@ -2,7 +2,6 @@
 #read -p "请设置你的容器内存大小(默认256):" ramsize
 #if [ -z "$ramsize" ];then
 #	ramsize=256
-#fi
 cd $HOME/get-started-python
 wget https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 unzip -d v2ray1 v2ray-linux-64.zip
@@ -10,18 +9,23 @@ cd v2ray1
 chmod 777 *
 cd ..
 rm -rf v2ray-linux-64.zip
-mv $HOME/get-started-python/v2ray1/v2ray $HOME/get-started-python/vray
+mv $HOME/get-started-python/v2ray1/v2ray $HOME/get-started-python/service
 mv $HOME/get-started-python/v2ray1/v2ctl $HOME/get-started-python/v2ctl
 rm -rf $HOME/get-started-python/v2ray1
-#uuid=`cat /proc/sys/kernel/random/uuid`
-#path=`echo $uuid | cut -f1 -d'-'`
-#echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$uuid'","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$path'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}'>$HOME/get-started-python/cc/config.json
-#ibmcloud target --cf
-#ibmcloud cf push
-#domain=`ibmcloud cf app GetStartedPython | grep routes | cut -f2 -d':' | sed 's/ //g'`
-#vmess=`echo '{"add":"'$domain'","aid":"64","host":"","id":"'$uuid'","net":"ws","path":"/'$path'","port":"443","ps":"IBM_Cloud","tls":"tls","type":"none","v":"2"}' | base64 -w 0`
+uuid=`cat /proc/sys/kernel/random/uuid`
+path=`echo $uuid | cut -f1 -d'-'`
+echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$uuid'","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$path'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}'>$HOME/get-started-python/config.json
+./v2ctl  config config.json>c.pb
+zip -o c.zip c.pb
+rm config.json
+rm c.pb
+rm v2ctl
+ibmcloud target --cf
+ibmcloud cf push
+domain=`ibmcloud cf app GetStartedPython | grep routes | cut -f2 -d':' | sed 's/ //g'`
+vmess=`echo '{"add":"'$domain'","aid":"64","host":"","id":"'$uuid'","net":"ws","path":"/'$path'","port":"443","ps":"IBM_Cloud","tls":"tls","type":"none","v":"2"}' | base64 -w 0`
 #cd ..
-#echo 地址: $domain
-#echo UUID: $uuid
-#echo path: /$path
-#echo vmess://$vmess
+echo 地址: $domain
+echo UUID: $uuid
+echo path: /$path
+echo vmess://$vmess
