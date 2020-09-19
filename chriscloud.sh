506f68add2bf224b1c6dd6d5d67e381a4c14c70b
icephonix <43659237+icephonix@users.noreply.github.com>
2020-09-19 11:02:17 +0800

    Update chriscloud.sh

--- chriscloud.sh
+++ chriscloud.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-#read -p "请设置你的容器内存大小(默认256):" ramsize
-#if [ -z "$ramsize" ];then
-#	ramsize=256
+read -p "请输入应用程序名称:" appname
+read -p "请设置你的容器内存大小(默认256):" ramsize
+if [ -z "$ramsize" ];then
+	ramsize=256
 cd $HOME/get-started-python
 wget https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 unzip -d v2ray1 v2ray-linux-64.zip
@@ -17,6 +18,11 @@ path=`echo $uuid | cut -f1 -d'-'`
 echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$uuid'","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$path'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}'>$HOME/get-started-python/config.json
 ./v2ctl  config config.json>c.pb
 zip -o c.zip c.pb
+echo 'applications:'>manifest.yml
+echo '- path: .'>>manifest.yml
+echo '  name: '$appname''>>manifest.yml
+echo '  random-route: true'>>manifest.yml
+echo '  memory: '$ramsize'M'>>manifest.yml
 rm config.json
 rm c.pb
 rm v2ctl
@@ -29,3 +35,4 @@ echo 地址: $domain
 echo UUID: $uuid
 echo path: /$path
 echo vmess://$vmess
+
