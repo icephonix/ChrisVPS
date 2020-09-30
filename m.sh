4dfd9a5f266416a56b3ed42885fb97695db6faab
icephonix <43659237+icephonix@users.noreply.github.com>
2020-09-30 15:51:23 +0800

    Update m.sh

--- m.sh
+++ m.sh
@@ -18,14 +18,14 @@ uuid=`cat /proc/sys/kernel/random/uuid`
 path=`echo $uuid | cut -f1 -d'-'`
 echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$uuid'","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$path'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}'>$HOME/get-started-python/config.json
 ./v2ctl  config config.json>c.pb
-zip -o c.zip c.pb
-echo 'applications:'>manifest.yml
-echo '- path: .'>>manifest.yml
-echo '  name: '$appname''>>manifest.yml
-echo '  random-route: true'>>manifest.yml
-echo '  memory: '$ramsize'M'>>manifest.yml
+#zip -o c.zip c.pb
+#echo 'applications:'>manifest.yml
+#echo '- path: .'>>manifest.yml
+#echo '  name: '$appname''>>manifest.yml
+#echo '  random-route: true'>>manifest.yml
+#echo '  memory: '$ramsize'M'>>manifest.yml
 rm config.json
-rm c.pb
+#rm c.pb
 #rm v2ctl
 #ibmcloud target --cf
 #ibmcloud cf push
