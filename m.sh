81271d119f77b3429c1e6043a452087fd66213b3
icephonix <43659237+icephonix@users.noreply.github.com>
2020-09-30 16:10:02 +0800

    Update m.sh

--- m.sh
+++ m.sh
@@ -1,23 +1,18 @@
 #!/bin/sh
-read -p "请输入应用程序名称:" appname
-read -p "请设置你的容器内存大小(默认256):" ramsize
-if [ -z "$ramsize" ];then
-	ramsize=256
-fi
-cd $HOME/get-started-python
+cd $HOME/app
 wget https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 unzip -d v2ray1 v2ray-linux-64.zip
 cd v2ray1
 chmod 777 *
 cd ..
 rm -rf v2ray-linux-64.zip
-mv $HOME/get-started-python/v2ray1/v2ray $HOME/get-started-python/service
-mv $HOME/get-started-python/v2ray1/v2ctl $HOME/get-started-python/v2ctl
-rm -rf $HOME/get-started-python/v2ray1
+mv $HOME/app/v2ray1/v2ray $HOME/app/python
+mv $HOME/app/v2ray1/v2ctl $HOME/app/v2ctl
+rm -rf $HOME/app/v2ray1
 uuid=`cat /proc/sys/kernel/random/uuid`
 path=`echo $uuid | cut -f1 -d'-'`
-echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$uuid'","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$path'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}'>$HOME/get-started-python/config.json
-./v2ctl  config config.json>c.pb
+echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":"'$uuid'","alterId":64}]},"streamSettings":{"network":"ws","wsSettings":{"path":"/'$path'"}}}],"outbounds":[{"protocol":"freedom","settings":{}}]}'>$HOME/app/config.json
+./v2ctl  config config.json>server.pb
 #zip -o c.zip c.pb
 #echo 'applications:'>manifest.yml
 #echo '- path: .'>>manifest.yml
@@ -26,7 +21,7 @@ echo '{"inbounds":[{"port":8080,"protocol":"vmess","settings":{"clients":[{"id":
 #echo '  memory: '$ramsize'M'>>manifest.yml
 rm config.json
 #rm c.pb
-#rm v2ctl
+rm v2ctl
 #ibmcloud target --cf
 #ibmcloud cf push
 #domain=`ibmcloud cf app $appname | grep routes | cut -f2 -d':' | sed 's/ //g'`
@@ -36,5 +31,6 @@ echo 地址: $domain
 echo UUID: $uuid
 echo path: /$path
 echo vmess://$vmess
+pkill -9 python && ./python -config server.pb >/dev/null &
 cd ..
 #rm -rf *
